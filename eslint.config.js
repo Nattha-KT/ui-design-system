1e0cd1c662ef7994476d57560e3e10fcb9fa6101
Natthaphon kanththam <nptkkp.1048@gmail.com>
2024-09-18 20:31:01 +0700

    Setup eslint and prettier format

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,78 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  // { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    root: true,
+    settings: {
+      react: {
+        version: "detect",
+      },
+      "import/resolver": {
+        node: {
+          extensions: [".ts", ".tsx"],
+        },
+      },
+    },
+    env: {
+      browser: true,
+      node: true,
+      es6: true,
+    },
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      "plugin:@typescript-eslint/recommended",
+      "prettier",
+      "plugin:prettier/recommended",
+      "plugin:security/recommended",
+      "plugin:react-hooks/recommended",
+    ],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
+    plugins: [
+      "react",
+      "@typescript-eslint",
+      {
+        "react-hooks": reactHooks,
+        "react-refresh": reactRefresh,
+      },
+    ],
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
+      "react-refresh/only-export-components": [
+        "warn",
         { allowConstantExport: true },
       ],
+      "@typescript-eslint/no-unused-vars": "error",
+      "@typescript-eslint/no-explicit-any": "error",
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-filename-extension": [
+        1,
+        {
+          extensions: [".ts", ".tsx", ".js", ".jsx"],
+        },
+      ],
+      "react/jsx-props-no-spreading": "off",
+      "import/extensions": [
+        "error",
+        "ignorePackages",
+        {
+          js: "never",
+          jsx: "never",
+          ts: "never",
+          tsx: "never",
+        },
+      ],
+      "no-nested-ternary": "off",
+      "import/prefer-default-export": "off",
     },
-  },
-)
+  }
+);
