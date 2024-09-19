154656ccf0f77247224203b1f6606fcf17917d3b
Natthaphon kanththam <nptkkp.1048@gmail.com>
2024-09-19 13:59:03 +0700

    check bad commit1

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit2 <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
