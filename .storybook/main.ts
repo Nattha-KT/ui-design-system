bf77351615456ea7fe32646a06d9276f567dc631
Natthaphon kanththam <nptkkp.1048@gmail.com>
2024-09-19 14:34:23 +0700

    Setup storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,40 @@
+import type { StorybookConfig } from '@storybook/react-vite';
+import path from 'path';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-onboarding',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
+  ],
+  core: {
+    builder: '@storybook/builder-vite', // 👈 The builder enabled here.
+  },
+  async viteFinal(config) {
+    // Merge custom configuration into the default config
+    config.plugins?.push(
+      /** @see https://github.com/aleclarson/vite-tsconfig-paths */
+      tsconfigPaths({
+        projects: [path.resolve(path.dirname(__dirname), 'tsconfig.json')],
+      }),
+    );
+    const { mergeConfig } = await import('vite');
+
+    return mergeConfig(config, {
+      // Add dependencies to pre-optimization
+      optimizeDeps: {
+        include: ['storybook-dark-mode'],
+      },
+    });
+  },
+
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+};
+export default config;
