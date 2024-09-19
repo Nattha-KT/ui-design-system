0e2a45c5779e90e96bd1ef56f31099c620b628c9
Natthaphon kanththam <nptkkp.1048@gmail.com>
2024-09-19 20:14:13 +0700

    Prepare tailwind custom style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+import animate from 'tailwindcss-animated';
+import radix_tailwind from 'tailwindcss-radix';
+
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        marble: {
+          50: '#F8FAFC',
+          100: '#F3F4F6',
+          200: '#E5E7EB',
+          300: '#CBD5E1',
+          400: '#94A3B8',
+          500: '#6B7280',
+          600: '#4B5563',
+          700: '#374151',
+          800: '#1F2937',
+          900: '#111827',
+        },
+      },
+    },
   },
-  plugins: [],
+  plugins: [animate, radix_tailwind],
+  darkMode: ['class'],
 };
