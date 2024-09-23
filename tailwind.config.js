4d0a7b25d95ddee71cc5ace80725dab24f11f547
Natthaphon kanththam <nptkkp.1048@gmail.com>
2024-09-23 13:38:18 +0700

    Implement shadcn and radix ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-import animate from 'tailwindcss-animated';
-import radix_tailwind from 'tailwindcss-radix';
-
+import { blackA, violet } from '@radix-ui/colors';
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
@@ -19,15 +17,109 @@ export default {
           800: '#1F2937',
           900: '#111827',
         },
+        ...violet,
+        ...blackA,
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
+        },
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+        },
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        chart: {
+          1: 'hsl(var(--chart-1))',
+          2: 'hsl(var(--chart-2))',
+          3: 'hsl(var(--chart-3))',
+          4: 'hsl(var(--chart-4))',
+          5: 'hsl(var(--chart-5))',
+        },
       },
-      backgroundImage: {
-        // btn_loading: 'url(./public/icons/loading-circle-white.png)',
+      backgroundImage: {},
+      keyframes: {
+        slideDownAndFade: {
+          from: {
+            opacity: '0',
+            transform: 'translateY(-2px)',
+          },
+          to: {
+            opacity: '1',
+            transform: 'translateY(0)',
+          },
+        },
+        slideLeftAndFade: {
+          from: {
+            opacity: '0',
+            transform: 'translateX(2px)',
+          },
+          to: {
+            opacity: '1',
+            transform: 'translateX(0)',
+          },
+        },
+        slideUpAndFade: {
+          from: {
+            opacity: '0',
+            transform: 'translateY(2px)',
+          },
+          to: {
+            opacity: '1',
+            transform: 'translateY(0)',
+          },
+        },
+        slideRightAndFade: {
+          from: {
+            opacity: '0',
+            transform: 'translateX(-2px)',
+          },
+          to: {
+            opacity: '1',
+            transform: 'translateX(0)',
+          },
+        },
       },
       animation: {
         'spin-slow': 'spin 1s linear infinite',
+        slideDownAndFade:
+          'slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+        slideLeftAndFade:
+          'slideLeftAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+        slideUpAndFade: 'slideUpAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+        slideRightAndFade:
+          'slideRightAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
       },
     },
   },
-  plugins: [animate, radix_tailwind],
+  plugins: [require('tailwindcss-radix'), require('tailwindcss-animate')],
   darkMode: ['class'],
 };
