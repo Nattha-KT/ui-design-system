3791d13837d902b9ca619e8b019a82c377635ba6
Natthaphon kanththam <nptkkp.1048@gmail.com>
2024-09-27 17:19:49 +0700

    Update name of component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
 export * from './button';
 export * from './indicator';
 export * from './input';
-export * from './password-input';
+export * from './password';
 export * from './text-button';
 export * from './textarea';
